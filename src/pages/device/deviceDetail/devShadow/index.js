fc183c876a977e3c64f911b5bf47a6c4aca4cdb2
ly <yang.li@clife.cn>
2021-08-04 15:47:17 +0800

    设备管理详情

--- src/pages/device/deviceDetail/devShadow/index.js
+++ src/pages/device/deviceDetail/devShadow/index.js
@@ -1,7 +1,76 @@
 import React, { useState } from 'react'
 import './index.scss'
+import DescWrapper from '../../../../components/desc-wrapper/DescWrapper';
+import { Tabs, Radio, Table } from 'antd';
+const { TabPane } = Tabs;
 export default function DeviceShadow() {
-    return (<div>
-        1
+    //下载
+    const downFile = () => {
+
+    }
+    const [dataSource, setDataSource] = useState([])
+
+    const columns = [
+        {
+            title: '数据名称',
+            dataIndex: 'name',
+            key: 'name',
+        },
+        {
+            title: '数据标识',
+            dataIndex: 'age',
+            key: 'age',
+        },
+        {
+            title: '数据类型',
+            dataIndex: 'address',
+            key: 'address',
+        },
+        {
+            title: '单位',
+            dataIndex: 'address',
+            key: 'address',
+        },
+        {
+            title: '数据属性',
+            dataIndex: 'address',
+            key: 'address',
+        },
+        {
+            title: '最新数据',
+            dataIndex: 'address',
+            key: 'address',
+        },
+        {
+            title: '操作',
+            dataIndex: 'address',
+            key: 'address',
+        },
+    ];
+    //筛选
+    const radioChange = (value) => {
+
+    }
+    return (<div id='device-shadow'>
+        <DescWrapper
+            style={{ marginBottom: 8, width: '100%', display: 'flex' }}
+            desc={['设备影子是设备最新状态在平台的缓存信息，您可以在平台实时查询设备的运行和状态信息，也可以通过API获取设备状态信息。详细说明可参考',
+                <a onClick={downFile}>帮助文档</a>]}>
+        </DescWrapper>
+        <Tabs defaultActiveKey='1' className='shadow-tab'>
+            <TabPane key={'1'} tab={'表单模式'}>
+                <div >
+                    <Radio.Group defaultValue="a" size="middle" onChange={radioChange} style={{ margin: '6px 0  22px 0' }}>
+                        <Radio.Button value="a">属性</Radio.Button>
+                        <Radio.Button value="b">事件</Radio.Button>
+                        <Radio.Button value="c">服务</Radio.Button>
+                    </Radio.Group>
+                    <Table dataSource={dataSource} columns={columns} />;
+                </div>
+            </TabPane>
+            <TabPane key={'2'} tab={'Json模式'}>
+                {/* <DevTag /> */}
+            </TabPane>
+        </Tabs>
     </div>)
 }
--- src/pages/device/deviceDetail/devTag/index.js
+++ src/pages/device/deviceDetail/devTag/index.js
@@ -1,7 +1,185 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useCallback } from 'react';
+import { Table, Input, InputNumber, Popconfirm, Form, Typography, Button, Space } from 'antd';
 import './index.scss'
-export default function DeviceTag() {
-    return (<div>
-        1
-    </div>)
+const originData = [];
+
+for (let i = 0; i < 5; i++) {
+    originData.push({
+        key: i.toString(),
+        name: `Edrward ${i}`,
+        age: 32,
+        address: `London Park no. ${i}`,
+    });
 }
+
+const EditableCell = ({
+    editing,
+    dataIndex,
+    title,
+    inputType,
+    record,
+    index,
+    children,
+    ...restProps
+}) => {
+    const inputNode = inputType === 'number' ? <InputNumber /> : <Input />;
+    return (
+        <td {...restProps}>
+            {editing ? (
+                <Form.Item
+                    name={dataIndex}
+                    style={{
+                        margin: 0,
+                        style:'100%'
+                    }}
+                    rules={[
+                        {
+                            required: true,
+                            message: `Please Input ${title}!`,
+                        },
+                    ]}
+                >
+                    {inputNode}
+                </Form.Item>
+            ) : (
+                children
+            )}
+        </td>
+    );
+};
+
+export default function EditableTable() {
+    const [form] = Form.useForm();
+    const [data, setData] = useState(originData);
+    const [editingKey, setEditingKey] = useState('');
+    const [actionData, setActionData] = useState({ actionType: 0, actionRecord: {}, actionLoading: false }) //actionType 0,没有操作，1 在测试环境操作,2 在生产环境操作,3 删除操作
+    const isEditing = (record) => record.key === editingKey;
+    useEffect(() => {
+    }, [])
+    const edit = (record) => {
+        form.setFieldsValue({
+            name: '',
+            age: '',
+            ...record,
+        });
+        setEditingKey(record.key);
+    };
+    const cancel = () => {
+        setEditingKey('');
+    };
+
+    const save = async (key) => {
+        try {
+            const row = await form.validateFields();
+            const newData = [...data];
+            const index = newData.findIndex((item) => key === item.key);
+
+            if (index > -1) {
+                const item = newData[index];
+                newData.splice(index, 1, { ...item, ...row });
+                setData(newData);
+                setEditingKey('');
+            } else {
+                newData.push(row);
+                setData(newData);
+                setEditingKey('');
+            }
+        } catch (errInfo) {
+            console.log('Validate Failed:', errInfo);
+        }
+    };
+    //新增
+    const handleEv = () => {
+        if (editingKey) {
+            return
+        }
+        let datas = JSON.parse(JSON.stringify(data))
+        let item = {
+            key: `${datas.length + 1}`,
+            name: `面对疾风吧`,
+            age: 9999,
+        }
+        datas.push(item)
+        setData(datas)
+        edit(item)
+    }
+    const columns = [
+        {
+            title: '标签Key',
+            dataIndex: 'name',
+            editable: true,
+        },
+        {
+            title: '标签Value',
+            dataIndex: 'age',
+            editable: true,
+        },
+        {
+            title: '操作',
+            dataIndex: 'operation',
+            render: (_, record) => {
+                const editable = isEditing(record);
+                return editable ? (
+                    <span>
+                        <a
+                            href="#!"
+                            onClick={() => save(record.key)}
+                            style={{
+                                marginRight: 8,
+                            }}
+                        >
+                            保存
+                        </a>
+                        <Popconfirm title="Sure to cancel?" onConfirm={cancel}>
+                            <a href="#!">取消</a>
+                        </Popconfirm>
+                    </span>
+                ) : (
+                    <Space>
+                        <Typography.Link disabled={editingKey !== ''} onClick={() => edit(record)}>
+                            编辑
+                        </Typography.Link>
+                        <Typography.Link disabled={editingKey !== ''}>
+                            删除
+                        </Typography.Link>
+                    </Space>
+                );
+            },
+        },
+    ];
+    const mergedColumns = columns.map((col) => {
+        if (!col.editable) {
+            return col;
+        }
+
+        return {
+            ...col,
+            onCell: (record) => ({
+                record,
+                inputType: 'text',
+                dataIndex: col.dataIndex,
+                title: col.title,
+                editing: isEditing(record),
+            }),
+        };
+    });
+
+    return (
+        <div id='device-tag'>
+            <Form form={form} component={false}>
+                <Table
+                    components={{
+                        body: {
+                            cell: EditableCell,
+                        },
+                    }}
+                    dataSource={data}
+                    columns={mergedColumns}
+                    rowClassName="editable-row"
+                    pagination={false}
+                />
+            </Form>
+            <Button type="primary" ghost className='edit-table-btn' onClick={() => { handleEv() }}>添加标签</Button>
+        </div>
+    );
+};
