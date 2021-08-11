516494519b66c1fe961e4dd17a179b9020d8b078
ly <yang.li@clife.cn>
2021-08-11 20:27:13 +0800

    自定义功能接口

--- src/pages/product/product-manage/product-edit/product-protocols/index.js
+++ src/pages/product/product-manage/product-edit/product-protocols/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, forwardRef, useImperativeHandle, useRef } from 'react'
+import ActionConfirmModal from '../../../../../components/action-confirm-modal/ActionConfirmModal';
 import moment from 'moment';
-import { Table, Button, Drawer } from 'antd';
+import { Table, Button, Drawer, Space } from 'antd';
 import './index.scss';
 import EditcusFn from './editcusFn'
 import Addfunction from './addModal'
@@ -10,47 +11,81 @@ import downpng from './../../../../../assets/images/product/download.png';
 import { post, Paths, get } from '../../../../../api';
 //处理数据
 function delaData(data) {
-    console.log(data,6666666666)
-    let newData=[]
+    let newData = []
     data.forEach(item => {
+        if (!item.funcParamList || !item.funcParamList.length) return
         item.funcParamList.forEach(item2 => {
-            let newItem=JSON.parse(JSON.stringify(item))
-            item2.nameSub=item2.name
-            delete item2.name
-            newData.push({...newItem,...item2})
+            let newItem = JSON.parse(JSON.stringify(item))
+            newData.push({ ...newItem, ...item2 })
         })
     })
-    console.log(newData,'========')
     return newData
 }
-function ProtocolFn({ nextStep,productId }, ref) {
+function ProtocolFn({ nextStep, productId }, ref) {
+    //删除弹窗
+    const [isDelVisible, setIsDelVisible] = useState(false)
+    const [delData, setDelData] = useState({})
+    //编辑右边抽屉
+    const openEditCus = (data) => {
+        setDestoryDom(true)
+        setTimeout(() => {
+            setRightEditVisible(true)
+        }, 0)
+    };
+    //打开删除弹窗
+    const openDel = (data) => {
+        setDelData(data)
+        setIsDelVisible(true)
+    }
+    const filterFn = (type, data) => {
+        let result = null
+        switch (type) {
+            case '数值':
+                result = `数值范围：${data.min}-${data.max},间距：${data.type},倍数：${data.type},单位：${data.type}`
+                break;
+            case '布尔':
+                result = `0：${data.type},1：${data.type}`
+                break;
+            case '枚举':
+                let value = ''
+                for (let key in data.propertyMap) {
+                    value += data.propertyMap[key] + ','
+                }
+                result = `枚举值：${value}`
+                break;
+            default:
+                return ''
+        }
+        return result
+    }
     const columns = [
-        { title: 'DP ID', dataIndex: 'name' },
-        { title: '功能类型', dataIndex: 'id' },
-        { title: '功能点名称', dataIndex: 'id' },
-        { title: '标识符', dataIndex: 'remark' },
-        { title: '参数名称', dataIndex: 'remark' },
-        { title: '参数标识', dataIndex: 'remark' },
+        { title: 'DP ID', dataIndex: 'key' },
+        { title: '功能类型', dataIndex: 'funcType' },
+        { title: '功能点名称', dataIndex: 'funcName' },
+        { title: '标识符', dataIndex: 'funcIdentifier' },
+        { title: '参数名称', dataIndex: 'name' },
+        { title: '参数标识', dataIndex: 'identifier' },
         {
             title: '数据传输类型', dataIndex: 'createTime',
-            render: text => <span>{text && moment(text).add(8, 'h').format('YYYY-MM-DD HH:mm:ss') || '--'}</span>
+            render: text => <span>1</span>
         },
-        { title: '数据类型', dataIndex: 'remark' },
-        { title: '数据属性', dataIndex: 'remark' },
+        { title: '数据类型', dataIndex: 'dataType' },
+        { title: '数据属性', dataIndex: 'propertyMap', render: (text, record) => <span>{filterFn(record.dataType, record)}</span> },
         {
-            title: '操作', dataIndex: 'remark', render: (text, record) => (
-                <><Button type="link">编辑</Button><Button type="link">删除</Button></>
+            title: '操作', render: (text, record) => (
+                <Space size="middle"><Button type="link" onClick={() => { openEditCus(record) }}>编辑</Button>
+                    <Button type="link" onClick={() => { openDel(record) }}>删除</Button></Space>
             ),
         },
     ];
-    const [dataSource, setdataSource] = useState([]);
+    const [cusData, setCusData] = useState([]);
+    const [standardData, setStandardData] = useState([]);
     const [selectId, setSelectId] = useState(0);
     //获取列表
-    const getList = () => {
-        let url=Paths.standardFnList+'/11759'
-        get('http://10.6.50.96:33330/product/dev/show/func/11759').then((res) => {
-            // console.log(res,'======')
-            delaData(res.standard)
+    const getList = (loading = true) => {
+        post(Paths.standardFnList, { productId: '11759' }, { loading }).then((res) => {
+            setStandardData(delaData(res.data.standard))
+            setCusData(delaData(res.data.custom))
         });
     }
     useEffect(() => {
@@ -61,14 +96,7 @@ function ProtocolFn({ nextStep,productId }, ref) {
     const [rightVisible, setRightVisible] = useState(false);
     const [rightEditVisible, setRightEditVisible] = useState(false);
     const [destoryDom, setDestoryDom] = useState(true);
-    //编辑右边抽屉
-    const openEditCus = () => {
-        setDestoryDom(true)
-        setTimeout(() => {
-            setRightEditVisible(true)
-        }, 0)
 
-    };
     //编辑抽屉关闭回调
     const onDestData = () => {
         setDestoryDom(false)
@@ -101,16 +129,23 @@ function ProtocolFn({ nextStep,productId }, ref) {
         nextStep()
     }
     //导出物模型
-    const exportFile=()=>{
-        let url=Paths.exportPhysicalModel+'/11759'
-        get(url, {}).then((res) => {
+    const exportFile = () => {
+        post(Paths.exportPhysicalModel, { productId: '11759' }).then((res) => {
             // delaData(res.data)
         });
     }
     useImperativeHandle(ref, () => ({
         onFinish: subNextConFirm
     }));
-    
+
+    //确定删除数据
+    const updateOkHandle = () => {
+        setIsDelVisible(false)
+    }
+    //取消删除数据
+    const updateCancelHandle = () => {
+        setIsDelVisible(false)
+    }
     const ref11 = useRef()
     return <div className='Protocol-wrap' ref={ref11}>
         <div className='Protocol-label'>
@@ -129,7 +164,7 @@ function ProtocolFn({ nextStep,productId }, ref) {
             <Table
                 rowKey="id"
                 columns={columns}
-                dataSource={dataSource}
+                dataSource={standardData}
             />
         </div>
         <div className='Protocol-download'>
@@ -145,14 +180,30 @@ function ProtocolFn({ nextStep,productId }, ref) {
             <Table
                 rowKey="id"
                 columns={columns}
-                dataSource={dataSource}
+                dataSource={cusData}
             />
         </div>
+        {/* 新增自定义 */}
         <NewCusmFn rightVisible={rightVisible} onCloseRight={onCloseRight}></NewCusmFn>
-        {/* <TitleEdit titleVisible={titleVisible} onCloseTitle={onCloseTitle}></TitleEdit> */}
+        {/* 编辑操作 */}
         {destoryDom && <EditcusFn rightVisible={rightEditVisible} onCloseRight={onCloseEditRight} destData={onDestData}></EditcusFn>}
-
+        {/* 新增标准 */}
         {isModalVisible && <Addfunction closeAdd={closeAdd} CancelAdd={CancelAdd} isModalVisible={isModalVisible}></Addfunction>}
+        {/* 删除操作 */}
+        {
+            isDelVisible && <ActionConfirmModal
+                visible={isDelVisible}
+                modalOKHandle={updateOkHandle}
+                modalCancelHandle={updateCancelHandle}
+                targetName={delData.funcName}
+                title='删除'
+                descGray={true}
+                needWarnIcon={true}
+                descText='确定删除此功能'
+            ></ActionConfirmModal>
+        }
+
     </div>
 }
+
 export default ProtocolFn = forwardRef(ProtocolFn)
