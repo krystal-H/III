c9f617fdbad4b203b54c2020471d75a58e774756
hcy <975328916@qq.com>
2021-09-15 16:10:01 +0800

    .

--- src/pages/product/product-manage/old-product-details/ProductDetails.js
+++ src/pages/product/product-manage/old-product-details/ProductDetails.js
@@ -3,12 +3,13 @@ import NoSourceWarn from '../../../../components/no-source-warn/NoSourceWarn';
 import PageTitle from '../../../../components/page-title/PageTitle';
 import ProductTabs from './product-tabs/ProductTabs';
 import {get,post,Paths} from '../../../../api';
+import { withRouter } from "react-router"
 import './ProductDetails.scss'
 
 // 获取路由中的ID参数
 const getProductIdFromPath = (match) => +match.params.id;
 
-export default function ProductDetail ({match}) {
+function ProductDetail ({match, history}) {
     const [productBaseInfo, setProductBaseInfo] = useState({})
 
     let productIdInRoutePath = getProductIdFromPath(match);
@@ -18,8 +19,10 @@ export default function ProductDetail ({match}) {
         if (productIdInRoutePath) {
             getBaseInfo(productIdInRoutePath)
         }
+    // eslint-disable-next-line no-use-before-define
     },[getBaseInfo,productIdInRoutePath])
 
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     const getBaseInfo = productId=>{
         post(Paths.getOldProductBaseInfo,{productId},{
             needVersion:1.1,
@@ -29,12 +32,16 @@ export default function ProductDetail ({match}) {
         })
     }
 
+    const goProductList = () => {
+        history.push('/open/product/proManage/list')
+    }
+
     if (!productIdInRoutePath) {
         return <NoSourceWarn tipText="没有传入产品ID哦"></NoSourceWarn>
     }
     return (
         <div className="old-pro-detail">
-            <PageTitle title={productBaseInfo.productName} titleBack={true}></PageTitle>
+            <PageTitle title={productBaseInfo.productName} titleBack={true} backHandle={() => {goProductList()}}></PageTitle>
             <div className={'content-wrapper'}>
                 <ProductTabs productId={productIdInRoutePath} productBaseInfo={productBaseInfo}
                 ></ProductTabs>
@@ -42,3 +49,5 @@ export default function ProductDetail ({match}) {
         </div>
     )
 }
+
+export default withRouter(ProductDetail)
