acd9a95a2d7e1e6581b0e4296c6242108ae25573
ly <yang.li@clife.cn>
2021-09-16 16:53:27 +0800

    确认面板默认图片

--- src/pages/product/product-manage/product-edit/firmpanel/index.js
+++ src/pages/product/product-manage/product-edit/firmpanel/index.js
@@ -30,11 +30,12 @@ function confirmModel({ nextStep }, ref) {
             productId = JSON.parse(sessionStorage.getItem('productItem')).productId
         }
         post(Paths.panelList, { productId }).then((res) => {
-            let data = res.data.list.filter(item => {
-                if (item.panelType == 3) {
-                    return item
-                }
-            })
+            // let data = res.data.list.filter(item => {
+            //     if (item.panelType == 3) {
+            //         return item
+            //     }
+            // })
+            let data = res.data.list
             let lastEst = {}
             if (!data.length) return;
             lastEst = data[0]
@@ -274,7 +275,7 @@ function confirmModel({ nextStep }, ref) {
                 <div className='confirm-pannel-content-right'>
                     <div>请使用“数联智能”App，扫描以下二维码，体验此面板。</div>
                     <div className='model-panal-code'>
-                        <img src={shoaLast.qrcode} alt='' />
+                        <img src={shoaLast.qrcode || demoAppOfficial} alt='' />
                     </div>
                     <div>
                         还没安装App？
