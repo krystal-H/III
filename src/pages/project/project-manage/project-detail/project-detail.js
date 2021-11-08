8008bb7fc84d4aad73f621979122fc6c13cc5e06
hcy <975328916@qq.com>
2021-11-08 14:43:53 +0800

    顶部信息

--- src/pages/project/project-manage/project-detail/project-detail.js
+++ src/pages/project/project-manage/project-detail/project-detail.js
@@ -5,22 +5,22 @@ import OpenAPI from './openAPI'
 import { getUrlParam } from '../../../../util/util';
 import React, { useState, useEffect, useMemo } from 'react'
 import { post, Paths, get } from '../../../../api';
-import PageTitle from '../../../../components/page-title/PageTitle';
-import { useHistory } from 'react-router-dom';
-import { Tabs } from 'antd';
+import PageTitle from '../../../../components/page-title/PageTitle'
+import { useHistory } from 'react-router-dom'
+import { Tabs } from 'antd'
 import './project-detail.scss'
 
-const { TabPane } = Tabs;
+const { TabPane } = Tabs
 
 export default function DeviceInfo({ match }) {
     let history = useHistory();
     const stepS = useMemo(() => {
         let step = getUrlParam('step') || '1'
         return step
-    }, []);
+    }, [])
     const devceId = match.params.id
     const tabCallback = value => {
-        history.push(match.url + '?' + `step=${value}`);
+        history.push(`${match.url}?step=${value}`)
     }
     const [baseInfo, setBaseInfo] = useState({})
     useEffect(() => {
@@ -32,13 +32,9 @@ export default function DeviceInfo({ match }) {
     return (<div id='device-detail'>
         <PageTitle backTitle='XXX' backHandle={() => { history.push('/open/project/projectManage/list') }}>
             <div className='device-top'>
-                <div className='device-top-item'>
-                    <label className='device-label'>设备ID：</label><span className='device-text'>{baseInfo.deviceUniqueId}</span>
-                    <span className='device-status'>{baseInfo.onlineStatus == 1 ? '在线' : '离线'}</span>
-                </div>
-                <div className='device-top-item'><label className='device-label'>物理地址ID：</label><span className='device-text'></span>{baseInfo.deviceMac}</div>
-                <div className='device-top-item'><label className='device-label'>产品名称：</label><span className='device-text'>{baseInfo.productName}</span></div>
-                <div className='device-top-item'><label className='device-label'>所属分类：</label><span className='device-text'>{baseInfo.productType}</span></div>
+                <div className='device-top-item'><label className='device-label'>项目ID：</label><span className='device-text'></span>123123132131</div>
+                <div className='device-top-item'><label className='device-label'>创建时间：</label><span className='device-text'>{baseInfo.productName}</span></div>
+                <div className='device-top-item'><label className='device-label'>更新时间：</label><span className='device-text'>{baseInfo.productType}</span></div>
             </div>
         </PageTitle>
         <div className='comm-shadowbox common-tab device-content'>
@@ -50,10 +46,10 @@ export default function DeviceInfo({ match }) {
                     <OpenAPI devceId={baseInfo.deviceId} />
                 </TabPane>
                 <TabPane key={'3'} tab={'分组'}>
-                    <Grouping devceId={baseInfo.deviceId} baseInfo={baseInfo}/>
+                    <Grouping devceId={baseInfo.deviceId} baseInfo={baseInfo} />
                 </TabPane>
                 <TabPane key={'4'} tab={'设备'}>
-                    <Device devceId={baseInfo.deviceId} baseInfo={baseInfo}/>
+                    <Device devceId={baseInfo.deviceId} baseInfo={baseInfo} />
                 </TabPane>
             </Tabs>
         </div>
