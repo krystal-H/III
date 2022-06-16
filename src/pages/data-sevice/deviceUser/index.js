c8d9445cdfe36cab46a21f6d83cc49e184ddc64a
Unknown <unknown@example.com>
2022-06-16 09:44:42 +0800

    打包

--- src/pages/data-sevice/deviceUser/index.js
+++ src/pages/data-sevice/deviceUser/index.js
@@ -38,13 +38,13 @@ const columns = [
     },
     {
         title: '新增设备用户次日留存数(昨日)',
-        dataIndex: 'newRatio',
-        key: 'newRatio',
+        dataIndex: 'newRetainNum',
+        key: 'newRetainNum',
     },
     {
         title: '活跃设备用户次日留存数(昨日)',
-        dataIndex: 'activeRatio',
-        key: 'activeRatio',
+        dataIndex: 'activeRetainNum',
+        key: 'activeRetainNum',
     },
     {
         title: '累计设备用户数',
